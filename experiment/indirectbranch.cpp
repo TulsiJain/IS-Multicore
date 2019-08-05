c9be5501c7e0be4ce142df3e581e3a1e032f0350
TulsiJain <tulsi.jain@delphix.com>
2019-08-05 10:34:44 -0700

    changes

--- experiment/indirectbranch.cpp
+++ experiment/indirectbranch.cpp
@@ -7,7 +7,7 @@
 pthread_mutex_t lock; 
 int list_size = 100000000;
 int thread_count;
-int array[list_size];
+int array[100000000];
 
 // Recall that an indirect branch is one for which the target must be determined 
 // at run-time; common examples being polymorphic code or a jump-table.  
@@ -18,49 +18,46 @@ int array[list_size];
 
 class Base {
 	public:
-  	virtual int Foo (int a) {
-  		return a + 1;
-  	};
+	virtual int Foo (int a) {
+		return a + 1;
+	};
 };
 
 class Derived : public Base {
 	public:
-  	int Foo (int a){
-  		return a + 2;
-  	};
+	int Foo (int a){
+		return a + 2;
+	};
 };
 
 void* multiThread_Handler (void *arg) 
 { 
-	  int thread_index = *((int *)arg);
+	int thread_index = *((int *)arg);
 
-   	pthread_mutex_lock(&lock); 
-   	unsigned int start_index = (thread_index*(list_size/thread_count));
-   	unsigned int end_index = ((thread_index+1) * (list_size/thread_count));
-   	pthread_mutex_unlock(&lock); 
+	pthread_mutex_lock(&lock); 
+	unsigned int start_index = (thread_index*(list_size/thread_count));
+	unsigned int end_index = ((thread_index+1) * (list_size/thread_count));
+	pthread_mutex_unlock(&lock); 
 
-   	for(int i =  start_index; i < end_index;  i++)
-   	{
-   		array[i] = array[i] + 1;
-   		Base* obj = new Derived;
+	for (int i =  start_index; i < end_index; i++) {
+		array[i] = array[i] + 1;
+		Base* obj = new Derived;
 		array[i] = obj->Foo(array[i]);
-   	}
-   	return NULL;
+	}
+	return NULL;
 } 
 
 int main(int argc, char *argv[]) 
 {
-  thread_count = atoi(argv[1]);
+	thread_count = atoi(argv[1]);
 	pthread_t threads[thread_count];
-    	for (int i = 0; i < thread_count; i++)
-	{
+	for (int i = 0; i < thread_count; i++) {
 		int *arg = (int *) malloc(sizeof(*arg));
-	    	*arg = i;
-	    	int error = pthread_create(&(threads[i]), NULL,
-	    	    multiThread_Handler, arg);
-		if (error != 0)
-		{
-	                printf("\nCan't create thread :[%s]", strerror(error));
+		*arg = i;
+		int error = pthread_create(&(threads[i]), NULL,
+		    multiThread_Handler, arg);
+		if (error != 0) {
+			printf("\nCan't create thread :[%s]", strerror(error));
 		}
 	}
 
@@ -68,5 +65,5 @@ int main(int argc, char *argv[])
 		pthread_join(threads[i], NULL);
 	}
 
-    exit(0); 
+	exit(0); 
 } 
